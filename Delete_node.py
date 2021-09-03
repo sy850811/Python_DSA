ca50f54e2cf27f57d00d448da4c2ce0f0f389bfc
sy850811 <89657362+sy850811@users.noreply.github.com>
2021-09-03 19:14:48 +0530

    Create  Delete_node.py
    You have been given a linked list of integers. Your task is to write a function that deletes a node from a given position, 'POS'.
    Note :
    
    Assume that the Indexing for the linked list always starts from 0.
    
    If the position is greater than or equal to the length of the linked list, you should return the same linked list without any change.
    
    Constraints :
    
    1 <= T <= 10^2
    0 <= N <= 10^5
    POS >= 0
    
    Time Limit: 1sec
    
    Sample Input 1 :
    
    1 
    3 4 5 2 6 1 9 -1
    3
    
    Sample Output 1 :
    
    3 4 5 6 1 9

---
+++ Delete_node.py
@@ -0,0 +1,117 @@
+
+from sys import stdin
+
+#Following is the Node class already written for the Linked List
+class Node :
+    def __init__(self, data) :
+        self.data = data
+        self.next = None
+
+def length(head):
+    count = 0
+    while head is not None:
+        count+=1
+        head = head.next
+    return count
+def deleteNode(head, pos) :
+    if head == None or pos >= length(head):
+        return head
+    ptr = 0
+    prev = None
+    curr = head
+    while pos != ptr:
+        prev = curr
+        curr = curr.next
+        ptr+=1
+    if prev == None:
+        head = head.next
+    else:
+        prev.next = curr.next
+    return head
+
+# def insertNode(head,pos,ele):
+#     if head == None or pos < 0:
+#         return head
+#     if (pos == 0):
+#         n = Node(ele)
+#         n.next = head
+#         head = n
+#         return head
+#     head.next = insertNode(head.next,pos-1,ele)
+#     return head
+    
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Taking Input Using Fast I/O
+def takeInput() :
+    head = None
+    tail = None
+
+    datas = list(map(int, stdin.readline().rstrip().split(" ")))
+
+    i = 0
+    while (i < len(datas)) and (datas[i] != -1) :
+        data = datas[i]
+        newNode = Node(data)
+
+        if head is None :
+            head = newNode
+            tail = newNode
+
+        else :
+            tail.next = newNode
+            tail = newNode
+
+        i += 1
+
+    return head
+
+
+
+#to print the linked list 
+def printLinkedList(head) :
+
+    while head is not None :
+        print(head.data, end = " ")
+        head = head.next
+
+    print()
+
+
+#main
+t = int(stdin.readline().strip())
+
+while t > 0 :
+    
+    head = takeInput()
+    pos = int(stdin.readline().rstrip())
+    
+    head = deleteNode(head, pos)
+    # head = insertNode(head,pos,1000)
+    printLinkedList(head)
+
+    t -= 1
